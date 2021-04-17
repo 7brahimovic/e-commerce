829fbdfb05e130516e6bf08b878a4c1020d2d2c5
7brahimovic <takuyakong@gmail.com>
2021-04-17 19:44:04 +0800

    ladies

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
+import Miyuki from '../../image/1.jpg';
+import Mai from '../../image/2.jpg';
+import Sarii from '../../image/3.jpg';
+import Rino from '../../image/4.jpg';
+import Yuki from '../../image/5.jpg';
+
 
 import './directory.styles.scss';
 
@@ -9,33 +15,33 @@ class Directory extends React.Component {
         this.state = {
             sections: [
                 {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    title: 'watanabe miyuki',
+                    imageUrl: Miyuki,
                     id: 1,
                     linkUrl: 'shop/hats'
                 },
                 {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    title: 'shiraishi mai',
+                    imageUrl: Mai,
                     id: 2,
                     linkUrl: 'shop/jackets'
                 },
                 {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    title: 'ikegami sarii',
+                    imageUrl: Sarii,
                     id: 3,
                     linkUrl: 'shop/sneakers'
                 },
                 {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    title: 'sashihara rino',
+                    imageUrl: Rino,
                     size: 'large',
                     id: 4,
                     linkUrl: 'shop/womens'
                 },
                 {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+                    title: 'kashiwagi yuki',
+                    imageUrl: Yuki,
                     size: 'large',
                     id: 5,
                     linkUrl: 'shop/mens'
