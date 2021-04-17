b29c45a6b7105e9d26c434d83053bb36339e7398
7brahimovic <takuyakong@gmail.com>
2021-04-17 15:54:33 +0800

    add a div

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -8,6 +8,13 @@ const HomePage = () => (
             <div className='menu-item'>
                 <div className='content'>
                     <h1 className='title'>HATS</h1>
+                    <span className='subtitle'>Sgit HOP NOW</span>
+                </div>
+            </div>
+            
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>HATS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
